ee127a799b02ca72f2a96e0fcb160740d86e9815
Peter Vaughan <160742536+cadalac-don@users.noreply.github.com>
2025-03-22 11:08:29 -0300

    Delete Assignment2 directory

--- Assignment2/OrderManagementApp/src/main/java/org/example/order/repository/PurchaseRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.order.repository;
-
-import org.example.order.model.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-
-}
-
